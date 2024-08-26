2cc3adbd7fd78626c78b515e3a656d415fc14f46
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-08-26 11:49:37 +0600

    complete reverse proxy

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
 func main() {
+
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /", Status)
+	target, _ := url.Parse("http://localhost:8082/")
+
+	proxy := &httputil.ReverseProxy{
+		Rewrite: func(pr *httputil.ProxyRequest) {
+			pr.SetURL(target)
+			pr.Out.Host = pr.In.Host
+		},
+	}
+
 	mux.HandleFunc("GET /ping", Ping)
+	mux.HandleFunc("GET /", proxy.ServeHTTP)
+	mux.HandleFunc("POST /", proxy.ServeHTTP)
 
 	log.Printf("server is running on port %v", 8080)
 
@@ -19,14 +31,6 @@ func main() {
 	if err != nil {
 		log.Fatal(`server crashed`)
 	}
-}
-
-func Status(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]any{
-		"ok":      true,
-		"message": "server is running",
-	})
 }
 
 func Ping(w http.ResponseWriter, r *http.Request) {
