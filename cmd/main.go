cadeac5fb7265bc0ab8c100a2c3c76dcda912194
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-08-26 10:59:17 +0600

    server

---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", Status)
+	log.Printf("server is running on port %v", 8080)
+
+	err := http.ListenAndServe(":8080", mux)
+
+	if err != nil {
+		log.Fatal(`server crashed`)
+	}
+}
+
+func Status(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]any{
+		"ok":      true,
+		"message": "server is running",
+	})
+}
